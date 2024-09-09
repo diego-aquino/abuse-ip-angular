1a66252b2da9ba5cdf93ba087867a6cb6c5b8134
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2024-09-08 22:35:26 -0300

    fix: remove unnecessary config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,6 @@
-const ignoredFilenamesWithoutExtension = ['.lintstagedrc', 'postcss.config', 'tailwind.config', 'next.config'];
+const ignoredFilenamesWithoutExtension = ['.lintstagedrc', 'postcss.config', 'tailwind.config'];
 
 module.exports = {
   '*': ['pnpm style:format'],
   [`**/!(${ignoredFilenamesWithoutExtension.join('|')})*.(j|t)s(x|)`]: ['pnpm lint'],
-  '*.prisma': ['pnpm prisma format'],
 };
