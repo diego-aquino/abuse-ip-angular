09c79eecdf453aeabaf4f1baceea2dcb3fbfdbf6
Daviiap <davii.sapiens@gmail.com>
2024-09-15 13:05:26 -0300

    feat: adding abuseIP request

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,17 +4,42 @@ import {
   IPSearchBarComponent,
 } from './components/ip-search-bar/ip-search-bar.component';
 import { IPSummaryComponent } from './components/ip-summary/ip-summary.component';
+import { AbuseIPData, AbuseIPRespondeI, AbuseIpService } from '@/app/services/abuse-ip.service';
+import { Subject } from 'rxjs';
+import { takeUntil, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [IPSearchBarComponent, IPSummaryComponent],
   templateUrl: './home.component.html',
+  providers: [AbuseIpService],
 })
 export class HomeComponent {
-  ip = '';
+  public ip = '';
+  public ipData: AbuseIPData | null;
+  private destroy$ = new Subject<void>();
+
+  constructor(private abuseIpService: AbuseIpService) {
+    this.ipData = null;
+  }
 
   onSearchBarSubmit(event: IPSearchFormValues) {
     this.ip = event.ip;
+
+    this.abuseIpService
+      .check(this.ip)
+      .pipe(
+        takeUntil(this.destroy$),
+        catchError((err) => {
+          console.error(err);
+          return [];
+        }),
+      )
+      .subscribe((response) => {
+        const abuseIPResponse = response as AbuseIPRespondeI;
+        this.ipData = abuseIPResponse.data;
+        console.log(this.ipData);
+      });
   }
 }
--- src/app/services/abuse-ip.service.ts
+++ src/app/services/abuse-ip.service.ts
@@ -2,6 +2,38 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@/environments/environment';
 
+export interface ReportI {
+  reportedAt: string;
+  comment: string;
+  categories: number[];
+  reporterId: number;
+  reporterCountryCode: string;
+  reporterCountryName: string;
+}
+
+export interface AbuseIPData {
+  ipAddress: string;
+  isPublic: boolean;
+  ipVersion: number;
+  isWhitelisted: boolean;
+  abuseConfidenceScore: number;
+  countryCode: string;
+  countryName: string;
+  usageType: string;
+  isp: string;
+  domain: string;
+  hostnames: string[];
+  isTor: boolean;
+  totalReports: number;
+  numDistinctUsers: number;
+  lastReportedAt: Date;
+  reports: ReportI[];
+}
+
+export interface AbuseIPRespondeI {
+  data: AbuseIPData;
+}
+
 @Injectable({
   providedIn: 'root',
 })
